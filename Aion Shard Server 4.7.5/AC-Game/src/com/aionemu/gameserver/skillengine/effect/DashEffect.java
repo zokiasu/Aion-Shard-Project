8b61cd937e9bea823f66f4b4a8e80efedd07345d
Zokiasu <32719751+Zokiasu@users.noreply.github.com>
2020-04-04 04:02:42 +0200

    Update DashEffect.java

--- Aion Shard Server 4.7.5/AC-Game/src/com/aionemu/gameserver/skillengine/effect/DashEffect.java
+++ Aion Shard Server 4.7.5/AC-Game/src/com/aionemu/gameserver/skillengine/effect/DashEffect.java
@@ -81,7 +81,7 @@ public class DashEffect extends DamageEffect {
             effect.getSkill().setTargetPosition(effected.getX(), effected.getY(), effected.getZ(), effected.getHeading());
         } else {
             log.warn("La target du joueur n'est pas la bonne");
-            effect.getSkill().setTargetPosition(effector.getActingCreature().getX(), effector.getActingCreature().getY(), effector.getActingCreature().getZ(), effector.getActingCreature().getHeading());
+            effect.getSkill().setTargetPosition(effector.getTarget().getX(), effector.getTarget().getY(), effector.getTarget().getZ(), effector.getTarget().getHeading());
         }
     }
 }
