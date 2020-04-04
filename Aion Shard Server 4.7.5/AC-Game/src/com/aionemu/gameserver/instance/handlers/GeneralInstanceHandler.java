0029dc63edef118e2b1d422c9516821557ac4480
Zokiasu <32719751+Zokiasu@users.noreply.github.com>
2020-04-05 00:39:09 +0200

    Test bastion avec de nouveau mob

--- Aion Shard Server 4.7.5/AC-Game/src/com/aionemu/gameserver/instance/handlers/GeneralInstanceHandler.java
+++ Aion Shard Server 4.7.5/AC-Game/src/com/aionemu/gameserver/instance/handlers/GeneralInstanceHandler.java
@@ -153,10 +153,8 @@ public class GeneralInstanceHandler implements InstanceHandler {
         NpcShoutsService.getInstance().sendMsg(instance, msg, Obj, isShout, color, time);
     }
     
-    protected void spawnWalk(final int npcId, final float x, final float y, final float z, final byte h, final int time,
-    				final String walkern) {
-    		ThreadPoolManager.getInstance().schedule(new Runnable() {
-
+    protected void spawnWalk(final int npcId, final float x, final float y, final float z, final byte h, final int time, final String walkern) {
+        ThreadPoolManager.getInstance().schedule(new Runnable() {
     		@Override
     		public void run() {
     			if (!isDestroyed()) {
