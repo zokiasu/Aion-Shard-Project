427e713694ca8c7bf029c6aac984f8a47f9f47e7
Zokiasu <32719751+Zokiasu@users.noreply.github.com>
2020-04-12 23:31:36 +0200

    Update ShopReloadStruct.java

--- Aion Shard Server 4.7.5/AC-Game/src/com/aionemu/gameserver/services/ecfunctions/shopreload/ShopReloadStruct.java
+++ Aion Shard Server 4.7.5/AC-Game/src/com/aionemu/gameserver/services/ecfunctions/shopreload/ShopReloadStruct.java
@@ -1,4 +1,4 @@
-package com.aionemu.gameserver.services.ecfunctions.oneVsone;
+package com.aionemu.gameserver.services.ecfunctions.shopreload;
 
 import com.aionemu.gameserver.configs.main.EventSystem;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
