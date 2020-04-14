bd5617f36f2c272ec8290913498b66e05289cce9
Zokiasu <32719751+Zokiasu@users.noreply.github.com>
2020-04-14 12:38:38 +0200

    Update cmd_reskin2.java

--- Aion Shard Server 4.7.5/AC-Game/data/scripts/system/handlers/playercommands/cmd_reskin2.java
+++ Aion Shard Server 4.7.5/AC-Game/data/scripts/system/handlers/playercommands/cmd_reskin2.java
@@ -173,7 +173,7 @@ public class cmd_reskin2 extends PlayerCommand {
                 item.setItemSkinTemplate(DataManager.ITEM_DATA.getItemTemplate(itemId));
                 admin.getInventory().decreaseByItemId(itemId, 1);
                 PacketSendUtility.sendMessage(admin, "Skin successfully changed!");
-                PacketSendUtility.sendMessage(admin, "The item " + item.getName() + " has deleted.");
+                PacketSendUtility.sendMessage(admin, DataManager.ITEM_DATA.getItemTemplate(itemId).getName() + " was deleted in your inventory.");
                 PacketSendUtility.sendMessage(admin, "For changing the skin, you have use " + toll + " Shard Coins!");
             }
 
