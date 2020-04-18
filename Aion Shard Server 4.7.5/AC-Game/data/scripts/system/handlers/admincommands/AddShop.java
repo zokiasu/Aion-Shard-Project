0f7b91ab76cffbf99a728478e0e3a0aadd83ab77
Zokiasu <32719751+Zokiasu@users.noreply.github.com>
2020-04-19 00:42:10 +0200

    Fix class Strings

--- Aion Shard Server 4.7.5/AC-Game/data/scripts/system/handlers/admincommands/AddShop.java
+++ Aion Shard Server 4.7.5/AC-Game/data/scripts/system/handlers/admincommands/AddShop.java
@@ -189,4 +189,26 @@ public class AddShop extends AdminCommand {
 
         return stringList;
     }
+
+    public class Strings {
+        private List<String> stringList = new ArrayList<String>();
+
+        public Strings () {}
+
+        public Strings(List<String> stringList) {
+            this.stringList = stringList;
+        }
+
+        public List<String> getStringList() {
+            return stringList;
+        }
+
+        public void setStringList(List<String> stringList) {
+            this.stringList = stringList;
+        }
+
+        public void addString(String string) {
+            stringList.add(string);
+        }
+    }
 }
