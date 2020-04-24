489b7ff02a45ae7f65dda0bb5da51d4ba14819ce
Zokiasu <32719751+Zokiasu@users.noreply.github.com>
2020-04-25 00:26:52 +0200

    Update GoldPackAction.java

--- Aion Shard Server 4.7.5/AC-Game/src/com/aionemu/gameserver/model/templates/item/actions/GoldPackAction.java
+++ Aion Shard Server 4.7.5/AC-Game/src/com/aionemu/gameserver/model/templates/item/actions/GoldPackAction.java
@@ -41,68 +41,112 @@ public class GoldPackAction extends AbstractItemAction {
         
         if(parentItem.getItemId() == 188710151){
             PacketSendUtility.sendMessage(player, "Ah que coucou.");
-            return;
-        }
-        if (!player.getInventory().decreaseByObjectId(parentItem.getObjectId(), 1)) {
-            return;
-        }
-        ItemTemplate itemTemplate = parentItem.getItemTemplate();
+            
+            if (!player.getInventory().decreaseByObjectId(parentItem.getObjectId(), 1)) {
+                return;
+            }
+            ItemTemplate itemTemplate = parentItem.getItemTemplate();
 
-        Connection con = null;
+            Connection con = null;
 
-        try {
-            final Timestamp deletionDate = new Timestamp(System.currentTimeMillis());
-            final Player player1 = player;
-            String LOGIN_DATABASE = GSConfig.LOGINSERVER_NAME;
-            Calendar cal = Calendar.getInstance();
+            try {
+                final Player player1 = player;
+                String LOGIN_DATABASE = GSConfig.LOGINSERVER_NAME;
+                int actualToll = 0;
+                con = DatabaseFactory.getConnection();
 
-            con = DatabaseFactory.getConnection();
-            PreparedStatement stmt = con.prepareStatement("UPDATE " + LOGIN_DATABASE +".account_data set "+ LOGIN_DATABASE +".account_data.membership = ? where " + LOGIN_DATABASE + ".account_data.name = ?");
-            stmt.setInt(1, 1);
-            stmt.setString(2, player1.getAcountName());
-            stmt.execute();
-            stmt.close();
+                PreparedStatement stmt1 = con.prepareStatement("SELECT * FROM " + LOGIN_DATABASE + ".account_data WHERE " + LOGIN_DATABASE + ".account_data.name = ?");
+                stmt1.setString(1, player1.getAcountName());
+                ResultSet resultSet = stmt1.executeQuery();
 
-            PreparedStatement stmt1 = con.prepareStatement("SELECT * FROM " + LOGIN_DATABASE + ".account_data WHERE " + LOGIN_DATABASE + ".account_data.name = ?");
-            stmt1.setString(1, player1.getAcountName());
-            ResultSet resultSet = stmt1.executeQuery();
+                if(resultSet.next()) {
+                    if(resultSet.getTimestamp("toll") != null) {
+                        actualToll = resultSet.getInt("toll");
+                    }
+                }
+                resultSet.close();
+                stmt1.close();
 
-            if(resultSet.next()) {
-                if(resultSet.getTimestamp("expire") != null) {
-                    cal.setTime(resultSet.getTimestamp("expire"));
-                    cal.add(Calendar.DAY_OF_WEEK, 30);
-                    deletionDate.setTime(cal.getTime().getTime());
+                PacketSendUtility.sendMessage(player, "You had " + actualToll + " coins.");
+                
+                actualToll = actualToll + 100;
+
+                PacketSendUtility.sendMessage(player, "With the 'Shard Coin' item you have won 100 coins.");
+                PacketSendUtility.sendMessage(player, "Now you have " + actualToll + " coins.");
+
+                PreparedStatement stmt2 = con.prepareStatement("UPDATE " + LOGIN_DATABASE +".account_data set "+ LOGIN_DATABASE +".account_data.toll = ? where " + LOGIN_DATABASE + ".account_data.name = ?");
+                stmt2.setInt(1, actualToll);
+                stmt2.setString(2, player1.getAcountName());
+                stmt2.execute();
+                stmt2.close();
+                con.close();
+            } catch (Exception e) {
+                return;
+            }
+
+            PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), parentItem.getObjectId(), itemTemplate.getTemplateId()), true);
+        } else {
+            if (!player.getInventory().decreaseByObjectId(parentItem.getObjectId(), 1)) {
+                return;
+            }
+            ItemTemplate itemTemplate = parentItem.getItemTemplate();
+
+            Connection con = null;
+
+            try {
+                final Timestamp deletionDate = new Timestamp(System.currentTimeMillis());
+                final Player player1 = player;
+                String LOGIN_DATABASE = GSConfig.LOGINSERVER_NAME;
+                Calendar cal = Calendar.getInstance();
+
+                con = DatabaseFactory.getConnection();
+                PreparedStatement stmt = con.prepareStatement("UPDATE " + LOGIN_DATABASE +".account_data set "+ LOGIN_DATABASE +".account_data.membership = ? where " + LOGIN_DATABASE + ".account_data.name = ?");
+                stmt.setInt(1, 1);
+                stmt.setString(2, player1.getAcountName());
+                stmt.execute();
+                stmt.close();
+
+                PreparedStatement stmt1 = con.prepareStatement("SELECT * FROM " + LOGIN_DATABASE + ".account_data WHERE " + LOGIN_DATABASE + ".account_data.name = ?");
+                stmt1.setString(1, player1.getAcountName());
+                ResultSet resultSet = stmt1.executeQuery();
+
+                if(resultSet.next()) {
+                    if(resultSet.getTimestamp("expire") != null) {
+                        cal.setTime(resultSet.getTimestamp("expire"));
+                        cal.add(Calendar.DAY_OF_WEEK, 30);
+                        deletionDate.setTime(cal.getTime().getTime());
+                    } else {
+                        cal.setTime(deletionDate);
+                        cal.add(Calendar.DAY_OF_WEEK, 30);
+                        deletionDate.setTime(cal.getTime().getTime());
+                    }
                 } else {
                     cal.setTime(deletionDate);
                     cal.add(Calendar.DAY_OF_WEEK, 30);
                     deletionDate.setTime(cal.getTime().getTime());
                 }
-            } else {
-                cal.setTime(deletionDate);
-                cal.add(Calendar.DAY_OF_WEEK, 30);
-                deletionDate.setTime(cal.getTime().getTime());
+                resultSet.close();
+                stmt1.close();
+
+                PreparedStatement stmt2 = con.prepareStatement("UPDATE " + LOGIN_DATABASE +".account_data set "+ LOGIN_DATABASE +".account_data.expire = ? where " + LOGIN_DATABASE + ".account_data.name = ?");
+                stmt2.setTimestamp(1, deletionDate);
+                stmt2.setString(2, player1.getAcountName());
+                stmt2.execute();
+                stmt2.close();
+
+                Date date = new Date(deletionDate.getTime());
+                DateFormat shortDateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+                PacketSendUtility.sendMessage(player, "Congratulations, you have just activated a new gold pack.");
+                PacketSendUtility.sendMessage(player, "Your next gold pack will need to be activated on : " + shortDateFormat.format(date));
+
+            } catch (Exception e) {
+                return;
             }
-            resultSet.close();
-            stmt1.close();
 
-            PreparedStatement stmt2 = con.prepareStatement("UPDATE " + LOGIN_DATABASE +".account_data set "+ LOGIN_DATABASE +".account_data.expire = ? where " + LOGIN_DATABASE + ".account_data.name = ?");
-            stmt2.setTimestamp(1, deletionDate);
-            stmt2.setString(2, player1.getAcountName());
-            stmt2.execute();
-            stmt2.close();
+            SystemMailService.getInstance().sendMail("GoldPack", player.getName(), "GoldPack Reward", "", 164002225, (long) 1, (long) 0, LetterType.BLACKCLOUD);
+            SystemMailService.getInstance().sendMail("GoldPack", player.getName(), "GoldPack Reward", "", 169610093, (long) 1, (long) 0, LetterType.BLACKCLOUD);
 
-            Date date = new Date(deletionDate.getTime());
-            DateFormat shortDateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
-            PacketSendUtility.sendMessage(player, "Congratulations, you have just activated a new gold pack.");
-            PacketSendUtility.sendMessage(player, "Your next gold pack will need to be activated on : " + shortDateFormat.format(date));
-
-        } catch (Exception e) {
-            return;
+            PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), parentItem.getObjectId(), itemTemplate.getTemplateId()), true);
         }
-
-        SystemMailService.getInstance().sendMail("GoldPack", player.getName(), "GoldPack Reward", "", 164002225, (long) 1, (long) 0, LetterType.BLACKCLOUD);
-        SystemMailService.getInstance().sendMail("GoldPack", player.getName(), "GoldPack Reward", "", 169610093, (long) 1, (long) 0, LetterType.BLACKCLOUD);
-
-        PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), parentItem.getObjectId(), itemTemplate.getTemplateId()), true);
     }
 }
