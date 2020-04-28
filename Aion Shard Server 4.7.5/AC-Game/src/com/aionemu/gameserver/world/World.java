5e494beafe671d84cc7e5288ccc1705a38964688
Zokiasu <32719751+Zokiasu@users.noreply.github.com>
2020-04-28 21:06:30 +0200

    Update World.java

--- Aion Shard Server 4.7.5/AC-Game/src/com/aionemu/gameserver/world/World.java
+++ Aion Shard Server 4.7.5/AC-Game/src/com/aionemu/gameserver/world/World.java
@@ -161,9 +161,9 @@ public class World {
         if (object instanceof Npc) {
             allNpcs.put(object.getObjectId(), (Npc) object);
         }
-        
+
         if (allObjects.put(object.getObjectId(), object) != null) {
-            throw new DuplicateAionObjectException();
+            //throw new DuplicateAionObjectException();
         }
     }
 
