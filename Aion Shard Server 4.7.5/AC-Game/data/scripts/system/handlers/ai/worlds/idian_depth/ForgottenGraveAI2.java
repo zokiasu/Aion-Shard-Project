5afe177881e1352fab068adf0f2252690358eb1e
Zokiasu <32719751+Zokiasu@users.noreply.github.com>
2020-04-30 03:28:37 +0200

    Edit config & NpcAi Idian Depth

--- Aion Shard Server 4.7.5/AC-Game/data/scripts/system/handlers/ai/worlds/idian_depth/ForgottenGraveAI2.java
+++ Aion Shard Server 4.7.5/AC-Game/data/scripts/system/handlers/ai/worlds/idian_depth/ForgottenGraveAI2.java
@@ -29,8 +29,10 @@
  */
 package ai.worlds.idian_depth;
 
+import com.aionemu.gameserver.ai2.AI2Actions;
 import com.aionemu.gameserver.ai2.AIName;
 import com.aionemu.gameserver.ai2.NpcAI2;
+import com.aionemu.gameserver.model.gameobjects.Creature;
 
 /**
  * @rework Ever
@@ -39,8 +41,8 @@ import com.aionemu.gameserver.ai2.NpcAI2;
 public class ForgottenGraveAI2 extends NpcAI2 {
 
     @Override
-    protected void handleDied() {
-        super.handleDied();
+    protected void handleAttack(Creature creature) {
+        super.handleAttack(creature);
         int owner = getOwner().getNpcId();
         int spawnNpc = 0;
         switch (owner) {
@@ -49,5 +51,7 @@ public class ForgottenGraveAI2 extends NpcAI2 {
                 break;
         }
         spawn(spawnNpc, getOwner().getSpawn().getX(), getOwner().getSpawn().getY(), getOwner().getSpawn().getZ(), getOwner().getSpawn().getHeading());
+		AI2Actions.deleteOwner(this);
+		AI2Actions.scheduleRespawn(this);
     }
 }
