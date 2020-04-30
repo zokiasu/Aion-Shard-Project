dcba068f1309e4a5b0eaefb7077d29f3686a515e
Zokiasu <32719751+Zokiasu@users.noreply.github.com>
2020-04-30 18:38:55 +0200

    Update ArmsfusionAI2.java

--- Aion Shard Server 4.7.5/AC-Game/data/scripts/system/handlers/ai/ArmsfusionAI2.java
+++ Aion Shard Server 4.7.5/AC-Game/data/scripts/system/handlers/ai/ArmsfusionAI2.java
@@ -7,6 +7,16 @@ import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 
+import com.aionemu.gameserver.ai2.AttackIntention;
+import com.aionemu.gameserver.ai2.event.AIEventType;
+import com.aionemu.gameserver.ai2.handler.*;
+import com.aionemu.gameserver.ai2.manager.SkillAttackManager;
+import com.aionemu.gameserver.dataholders.DataManager;
+import com.aionemu.gameserver.model.gameobjects.Creature;
+import com.aionemu.gameserver.model.gameobjects.VisibleObject;
+import com.aionemu.gameserver.model.skill.NpcSkillEntry;
+import com.aionemu.gameserver.model.templates.npcshout.ShoutEventType;
+
 @AIName("armsfusion")
 public class ArmsfusionAI2 extends NpcAI2 {
 
